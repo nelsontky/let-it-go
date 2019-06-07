4e43c306b3a74d665050057028bde86c35f09e26
Nelson <nelson@ccb.wtf>
2019-06-08 03:20:30 +0800

    Added state in main App component in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,29 +3,38 @@ import Layout from "../components/layout"
 import ***REMOVED*** graphql ***REMOVED*** from "gatsby"
 import ***REMOVED*** Link ***REMOVED*** from "gatsby"
 
-export default (***REMOVED*** data ***REMOVED***) => ***REMOVED***
-  const edges = data.allToilets.edges
-  return (
-    <Layout>
-      <h1>Let It Go</h1>
-      <table style=***REMOVED******REMOVED*** tableLayout: "fixed" ***REMOVED******REMOVED***>
-        <thead>
-          <tr>
-            <th>Name</th>
-          </tr>
-        </thead>
-        <tbody>
-          ***REMOVED***edges.map((***REMOVED*** node ***REMOVED***, index) => (
-            <tr key=***REMOVED***index***REMOVED***>
-              <td><Link
-                to=***REMOVED***"/" + node.name.replace(/\s/g,'')***REMOVED***>***REMOVED***node.name***REMOVED***
-              </Link></td>
+export default class App extends React.Component ***REMOVED***
+
+  constructor(props) ***REMOVED***
+    super(props)
+
+    this.state = ***REMOVED******REMOVED***
+  ***REMOVED***
+
+  render() ***REMOVED***
+    const edges = this.props.data.allToilets.edges
+    return (
+      <Layout>
+        <h1>Let It Go</h1>
+        <table style=***REMOVED******REMOVED*** tableLayout: "fixed" ***REMOVED******REMOVED***>
+          <thead>
+            <tr>
+              <th>Name</th>
             </tr>
-          ))***REMOVED***
-        </tbody>
-      </table>
-    </Layout>
-  )
+          </thead>
+          <tbody>
+            ***REMOVED***edges.map((***REMOVED*** node ***REMOVED***, index) => (
+              <tr key=***REMOVED***index***REMOVED***>
+                <td><Link
+                  to=***REMOVED***"/" + node.name.replace(/\s/g,'')***REMOVED***>***REMOVED***node.name***REMOVED***
+                </Link></td>
+              </tr>
+            ))***REMOVED***
+          </tbody>
+        </table>
+      </Layout>
+    )
+  ***REMOVED***
 ***REMOVED***
 
 export const query = graphql`
