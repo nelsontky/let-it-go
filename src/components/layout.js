fb0dd527c09610166b0fda6ac6ab8e9d85689d75
Nelson <nelson@ccb.wtf>
2019-06-09 22:34:30 +0800

    Added back button

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,10 +3,8 @@ import ***REMOVED*** Link ***REMOVED*** from "gatsby"
 
 export default (props) => (
   <div style=***REMOVED******REMOVED*** margin: `3rem auto`, maxWidth: 650, padding: `0 1rem` ***REMOVED******REMOVED***>
-    <tr style=***REMOVED******REMOVED*** border: `none` ***REMOVED******REMOVED***>
-      ***REMOVED***!props.main && <td><Link to="/">Back</Link></td>***REMOVED***
-      <td><h3>Let It Go</h3></td>
-    </tr>
+    ***REMOVED***!props.main && <Link to="/">Back</Link>***REMOVED***
+    ***REMOVED***props.main && <h1>Let It Go</h1>***REMOVED***
     ***REMOVED***props.children***REMOVED***
   </div>
 )
