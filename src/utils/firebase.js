167f74d961b351941fb1a292a347c9d8f5080c13
Nelson <nelson@ccb.wtf>
2019-07-03 10:52:39 +0800

    Updated project url

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,17 +1,15 @@
 var firebase = require("firebase/app")
 require("firebase/auth")
 require("firebase/firestore")
-
 const firebaseConfig = {
   apiKey: `${process.env.GATSBY_GOOGLE_MAPS_API}`,
-  authDomain: "let-it-go-ce90f.firebaseapp.com",
-  databaseURL: "https://let-it-go-ce90f.firebaseio.com",
-  projectId: "let-it-go-ce90f",
-  storageBucket: "let-it-go-ce90f.appspot.com",
-  messagingSenderId: "243397254091",
-  appId: "1:243397254091:web:bede1631a9ce7f43"
+  authDomain: "let-it-go-e6229.firebaseapp.com",
+  databaseURL: "https://let-it-go-e6229.firebaseio.com",
+  projectId: "let-it-go-e6229",
+  storageBucket: "let-it-go-e6229.appspot.com",
+  messagingSenderId: "917575844853",
+  appId: "1:917575844853:web:88ec0cbe059a0d43"
 };
-
 let instance
 
 class Firebase {
