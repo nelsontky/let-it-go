ec065150fa8b8af7130e71690118ce5928b666cf
Nelson <nelson@ccb.wtf>
2019-07-07 20:35:07 +0800

    Restricted to 1 review per user, allow for editing of reviews

--- src/components/reviews.js
+++ src/components/reviews.js
@@ -6,8 +6,6 @@ import Stars from "../components/stars"
 import StarsStatic from "../components/starsStatic"
 import PaginatedArray from "../components/paginatedArray"
 
-const uniqid = require("uniqid")
-
 class ReviewText extends React.Component {
   constructor(props) {
     super(props)
@@ -55,23 +53,78 @@ class ReviewText extends React.Component {
   }
 }
 
+// this.state.isSignedIn &&
+//   this.props.review.uid === this.props.auth.currentUser.uid && (
+// <span style={{ color: "gray", fontSize: "80%" }}>
+//   {" • "}
+//   <button
+//     onClick={this.handleEdit}
+//     style={utils.buttonToLinkStyle("red", "100%")}
+//   >
+//     Edit
+//   </button>
+// </span>
+//   )
+
+class SingleReview extends React.Component {
+  render() {
+    return (
+      <span>
+        <img
+          src={this.props.children.photoURL}
+          alt={this.props.children.name}
+          width={36}
+          height={36}
+          style={{ verticalAlign: "top" }}
+        />
+        <strong style={{ color: "blue", fontSize: "80%" }}>
+          {" " + this.props.children.name}
+        </strong>
+        {/* Edit button (if shown) */}
+        {this.props.handleEdit != null && (
+          <span style={{ color: "gray", fontSize: "80%" }}>
+            {" • "}
+            <button
+              onClick={this.props.handleEdit}
+              style={utils.buttonToLinkStyle("red", "100%")}
+            >
+              Edit
+            </button>
+          </span>
+        )}
+        <br />
+        <span>
+          <StarsStatic score={this.props.children.score} />
+          <span style={{ color: "gray", fontSize: "80%" }}>
+            {" " + utils.howLongAgo(this.props.children.date)}
+          </span>
+        </span>
+        <br />
+        <ReviewText review={this.props.children.review} />
+      </span>
+    )
+  }
+}
+
 class Reviews extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       reviews: [],
+      otherReviews: [],
       isSignedIn: false,
       myReview: "",
       reviewsLoading: true,
       starsState: [false, false, false, false, false],
       pageNumber: 1,
+      inputBoxShown: true,
     }
     this.starsKey = 0
     this.paginateKey = 0
 
     this.handleChange = this.handleChange.bind(this)
     this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleDelete = this.handleDelete.bind(this)
+    this.handleEdit = this.handleEdit.bind(this)
     this.handleStarClick = this.handleStarClick.bind(this)
     this.handleSort = this.handleSort.bind(this)
     this.handlePageChange = this.handlePageChange.bind(this)
@@ -94,12 +147,20 @@ class Reviews extends React.Component {
       .onSnapshot(querySnapshot => {
         let reviews = []
         querySnapshot.forEach(doc => {
-          reviews = doc.data().userReviews.concat(reviews)
+          reviews.push(doc.data().review)
         })
         reviews.sort((a, b) => b.date.toDate() - a.date.toDate())
         this.setState({
           reviews,
           reviewsLoading: false,
+          inputBoxShown:
+            this.auth.currentUser != null &&
+            reviews.filter(x => x.uid === this.auth.currentUser.uid).length !==
+              1,
+          otherReviews:
+            this.auth.currentUser == null
+              ? reviews
+              : reviews.filter(x => x.uid !== this.auth.currentUser.uid),
         })
       })
 
@@ -115,7 +176,15 @@ class Reviews extends React.Component {
     this.firebase.ui.start("#firebaseui-auth-container", this.uiConfig)
 
     this.stopAuthListener = this.auth.onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user })
+      this.setState({
+        isSignedIn: !!user,
+        otherReviews:
+          this.auth.currentUser == null
+            ? this.state.reviews
+            : this.state.reviews.filter(
+                x => x.uid !== this.auth.currentUser.uid
+              ),
+      })
     })
   }
 
@@ -131,20 +200,16 @@ class Reviews extends React.Component {
       .doc(this.props.name)
       .collection("users")
       .doc(this.auth.currentUser.uid)
-      .set(
-        {
-          userReviews: this.firebase.firebase.firestore.FieldValue.arrayUnion({
-            id: uniqid(),
-            name: this.auth.currentUser.displayName,
-            photoURL: this.auth.currentUser.photoURL,
-            review: this.state.myReview,
-            date: new Date(),
-            uid: this.auth.currentUser.uid,
-            score: utils.starsStateToScore(this.state.starsState),
-          }),
+      .set({
+        review: {
+          name: this.auth.currentUser.displayName,
+          photoURL: this.auth.currentUser.photoURL,
+          review: this.state.myReview,
+          date: new Date(),
+          uid: this.auth.currentUser.uid,
+          score: utils.starsStateToScore(this.state.starsState),
         },
-        { merge: true }
-      )
+      })
 
     this.setState({
       myReview: "",
@@ -154,24 +219,19 @@ class Reviews extends React.Component {
     })
   }
 
-  handleDelete(id) {
-    if (window.confirm(`Are you sure you want to delete your review?`)) {
-      this.db
-        .collection("reviews")
-        .doc(this.props.name)
-        .collection("users")
-        .doc(this.auth.currentUser.uid)
-        .set({
-          userReviews: this.state.reviews.filter(
-            x => x.uid === this.auth.currentUser.uid && x.id !== id
-          ),
-        })
+  handleEdit() {
+    console.log("edit")
+    const review = this.state.reviews.filter(
+      x => x.uid === this.auth.currentUser.uid
+    )[0]
 
-      this.setState({
-        pageNumber: 1,
-        sortBy: "newest",
-      })
-    }
+    this.setState({
+      myReview: review.review,
+      starsState: [true, true, true, true, true].map(
+        (x, i) => i < review.score
+      ),
+      inputBoxShown: true,
+    })
   }
 
   handleStarClick(event) {
@@ -198,6 +258,10 @@ class Reviews extends React.Component {
       reviews: reviewsCopy,
       sortBy: event.target.value,
       pageNumber: 1,
+      otherReviews:
+        this.auth.currentUser == null
+          ? reviewsCopy
+          : reviewsCopy.filter(x => x.uid !== this.auth.currentUser.uid),
     })
   }
 
@@ -235,6 +299,7 @@ class Reviews extends React.Component {
             </p>
           </div>
         )}
+        {/* Signed in message */}
         {this.state.isSignedIn && !this.state.reviewsLoading && (
           <div>
             <p>
@@ -249,31 +314,52 @@ class Reviews extends React.Component {
                 Sign-out
               </button>
             </p>
-            <form onSubmit={this.handleSubmit}>
-              <Stars
-                handleStarClick={this.handleStarClick}
-                starsState={this.state.starsState}
-                key={this.starsKey++}
-              />
-              <textarea
-                style={{ width: "100%", resize: "none", height: "75px" }}
-                value={this.state.myReview}
-                onChange={this.handleChange}
-                placeholder="Write a review..."
-              />
-              <input
-                type="submit"
-                value="Submit"
-                disabled={
-                  this.state.myReview.trim() === "" ||
-                  utils.starsStateToScore(this.state.starsState) === 0
-                }
-              />
-            </form>
+
+            {/* Comment input box/My review */}
+            {this.state.inputBoxShown && !this.state.reviewsLoading ? (
+              <form onSubmit={this.handleSubmit}>
+                <Stars
+                  handleStarClick={this.handleStarClick}
+                  starsState={this.state.starsState}
+                  key={this.starsKey++}
+                />
+                <textarea
+                  style={{ width: "100%", resize: "none", height: "75px" }}
+                  value={this.state.myReview}
+                  onChange={this.handleChange}
+                  placeholder="Write a review..."
+                />
+                <input
+                  type="submit"
+                  value="Submit"
+                  disabled={
+                    this.state.myReview.trim() === "" ||
+                    utils.starsStateToScore(this.state.starsState) === 0
+                  }
+                />
+              </form>
+            ) : (
+              <div
+                style={{
+                  borderStyle: "solid",
+                  borderColor: "grey",
+                  borderWidth: "1px",
+                }}
+              >
+                <SingleReview handleEdit={this.handleEdit}>
+                  {
+                    this.state.reviews.filter(
+                      x => x.uid === this.auth.currentUser.uid
+                    )[0]
+                  }
+                </SingleReview>
+              </div>
+            )}
           </div>
         )}
         {/* Sorting dropdown */}
-        {!this.state.reviewsLoading && this.state.reviews.length !== 0 && (
+        <br />
+        {!this.state.reviewsLoading && this.state.otherReviews.length !== 0 && (
           <div>
             <label>
               Sort by:{" "}
@@ -287,7 +373,7 @@ class Reviews extends React.Component {
         )}
         {/* Comments start here */}
         <table style={{ tableLayout: "fixed" }}>
-          {this.state.reviews.length === 0 ? (
+          {this.state.otherReviews.length === 0 ? (
             <tbody>
               <tr key={1}>
                 <td>
@@ -307,41 +393,11 @@ class Reviews extends React.Component {
               handlePageChange={this.handlePageChange}
               columns={1}
             >
-              {this.state.reviews.map((x, i) => {
+              {this.state.otherReviews.map((x, i) => {
                 return (
                   <tr key={i}>
                     <td>
-                      <img
-                        src={x.photoURL}
-                        alt={x.name}
-                        width={36}
-                        height={36}
-                        style={{ verticalAlign: "top" }}
-                      />
-                      <strong style={{ color: "blue", fontSize: "80%" }}>
-                        {" " + x.name}
-                      </strong>
-                      {this.state.isSignedIn &&
-                        x.uid === this.auth.currentUser.uid && (
-                          <span style={{ color: "gray", fontSize: "80%" }}>
-                            {" • "}
-                            <button
-                              onClick={() => this.handleDelete(x.id)}
-                              style={utils.buttonToLinkStyle("red", "100%")}
-                            >
-                              Delete
-                            </button>
-                          </span>
-                        )}
-                      <br />
-                      <span>
-                        <StarsStatic score={x.score} />
-                        <span style={{ color: "gray", fontSize: "80%" }}>
-                          {" " + utils.howLongAgo(x.date)}
-                        </span>
-                      </span>
-                      <br />
-                      <ReviewText key={x.id} review={x.review} />
+                      <SingleReview>{x}</SingleReview>
                     </td>
                   </tr>
                 )
